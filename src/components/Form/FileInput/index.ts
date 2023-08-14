69952f544667ab2786c45c611400570c6890b433
leandrotune <leandrotune@pop-os.localdomain>
2023-08-14 14:00:58 -0300

    Listagem de arquivos

--- src/components/Form/FileInput/index.ts
+++ src/components/Form/FileInput/index.ts
@@ -1,6 +1,7 @@
 import { Control } from './Control'
 import { ImagePreview } from './ImagePreview'
 import { Trigger } from './Trigger'
+import { FileList } from './FileList'
 import { Root } from './Root'
 
-export { Control, ImagePreview, Trigger, Root }
+export { Control, ImagePreview, Trigger, FileList, Root }
