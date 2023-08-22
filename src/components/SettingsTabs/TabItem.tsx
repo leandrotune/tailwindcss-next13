97bdbaadb2ea6905d59f5b11bbab1efc612314ba
leandrotune <leandrotune@pop-os.localdomain>
2023-08-21 21:12:42 -0300

    Criando efeitos de foco

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -10,16 +10,18 @@ interface TabItemProps {
 export function TabItem({ value, title, isSelected = false }: TabItemProps) {
   return (
     <Tabs.Trigger
-      className="relative px-1 pb-4 text-sm font-medium text-zinc-500 data-[state=active]:text-violet-700"
+      className="group relative px-1 pb-4 text-sm font-medium text-zinc-500 outline-none data-[state=active]:text-violet-700"
       value={value}
     >
-      <span>{title}</span>
+      <span className="rounded group-focus-visible:ring-1 group-focus-visible:ring-violet-400 group-focus-visible:ring-offset-4">
+        {title}
+      </span>
 
       {isSelected && (
         <motion.div
           layoutId="activeTab"
-          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700 "
-        ></motion.div>
+          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700"
+        />
       )}
     </Tabs.Trigger>
   )
